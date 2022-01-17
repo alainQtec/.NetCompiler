d12cc20b70cb9f2fc8264f813029b728d2a1eacb
Alain Herve <alain.1337dev@outlook.com>
2022-01-17 17:46:00 +0200

    Create Get-Roslyn.ps1

---
+++ Functions/Get-Roslyn.ps1
@@ -0,0 +1,24 @@
+function Get-RoslynCompiler {
+  [CmdletBinding()]
+  param (
+    
+  )
+  
+  begin {
+    # $Params = @{
+    #   InstallCommad  = "Install-Package Microsoft.CodeDom.Providers.DotNetCompilerPlatform -Version 3.6.0"
+    #   GitHubRepo     = "https://github.com/aspnet/RoslynCodeDomProvider"
+    #   GithubReleases = "https://github.com/aspnet/RoslynCodeDomProvider/releases"
+    #   ProjectSite    = "https://www.asp.net/"
+    # }
+    # $params
+  }
+  
+  process {
+    
+  }
+  
+  end {
+    
+  }
+}
